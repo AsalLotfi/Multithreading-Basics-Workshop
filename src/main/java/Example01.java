d986f5528fe1df8b9737ed9e6163ab1f909c274d
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-27 14:15:16 +0330

    feat: add Example01

---
+++ src/main/java/Example01.java
@@ -0,0 +1,10 @@
+public class Example01 {
+    //TODO(1): Create a class that extends Thread class.
+    //TODO(2): print greetings in the Thread class
+    //TODO(4): repeat the greetings using a loop
+    public static void main(String[] args)
+    {
+        //TODO(3): Create an object of the class you created, and call start()
+        //TODO(0): Write a greeting from the current thread
+    }
+}
