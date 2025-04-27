1eb80592ccf6298f7a0ba7d6db02f58962cd77c1
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-27 14:17:47 +0330

    feat: add Example02

---
+++ src/main/java/Example02.java
@@ -0,0 +1,8 @@
+public class Example02 {
+    //TODO(1): Create a class that implements the Runnable interface
+    public static void main(String[] args)
+    {
+        //TODO(2): start a thread that runs the Runnable
+        //TODO(0): print 20 greetings
+    }
+}
