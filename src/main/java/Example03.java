47b28b3c36d2eb132a45b114b7616a2ac8453564
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2025-04-27 14:35:07 +0330

    feat: add Example03

---
+++ src/main/java/Example03.java
@@ -0,0 +1,14 @@
+public class Example03 {
+    //TODO(1): create a class that implements the Runnable interface
+    //TODO(2): print greetings in a loop
+    //TODO(5): add a way of ending the thread when it is interrupted
+    //TODO(6): add sleep in the loop
+    //TODO(7): handle InterruptedException
+    public static void main(String[] args)
+    {
+        //TODO(0): print greetings in a loop
+        //TODO(3): talk about thread.stop...
+        //TODO(4): interrupt the other thread at some time
+        //TODO(8): observe thread state
+    }
+}
